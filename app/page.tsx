2cc4044b902a9fcea8a04634e97cc09503fd1e7f
Tejas Rastogi <tejasrastogi456@gmail.com>
2025-07-13 11:31:36 +0700

    Complete Navbar component and guest, about and contact pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-export default function HomePage() {
+import Guest from '@/components/Guest';
+import { currentUser } from '@clerk/nextjs/server';
+
+export default async function HomePage() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
+
   return (
-    <div className='text-red-400'>HomePage</div>
-  )
+    <div className='text-red-400 dark:text-red-300'>
+      <h1>HomePage</h1>
+    </div>
+  );
 }
